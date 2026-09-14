d007e5c89d8993e401235d05936b827a2a02fb17
QuvonchbekQosimov <qosimovquvonc@gmail.com>
2026-09-14 15:48:53 +0500

    loyihaga o'zgarishlar kiritild1i2

--- BooksCatalog/catalog/views.py
+++ BooksCatalog/catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.select_related('category').all()
+    context = {
+        'books': books
+    }
+    return render(request, 'catalog/book_list.html', context)
+
+
+
+
