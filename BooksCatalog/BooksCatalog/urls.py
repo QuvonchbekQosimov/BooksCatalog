87353509dfc51b4e71daaa1968a2821098484d7b
QuvonchbekQosimov <qosimovquvonc@gmail.com>
2026-09-15 10:58:33 +0500

    loyihaga o'zgarishlar kiritild1i22wfarg3

--- BooksCatalog/BooksCatalog/urls.py
+++ BooksCatalog/BooksCatalog/urls.py
@@ -1,4 +1,4 @@
-from catalog.views import book_detail, book_list
+from catalog.views import book_detail, book_list, register
 from django.contrib import admin
 from django.urls import path
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', book_list, name='book_list'),  
     path('books/<int:book_id>/', book_detail, name='book_detail'),
+    path('register/', register, name='register'),
 ]
--- BooksCatalog/catalog/views.py
+++ BooksCatalog/catalog/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Book
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render, get_object_or_404
+
+from catalog.models import Book
+
 
 def book_list(request):
     books = Book.objects.select_related('category').all()
@@ -14,6 +18,20 @@ def book_detail(request, book_id):
         'book': book
     }
     return render(request, 'catalog/book_detail.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book_list')
+    else:
+        form = UserCreationForm()
+
+
+
+    return render(request, 'catalog/register.html', {'form': form})
 
 
 
